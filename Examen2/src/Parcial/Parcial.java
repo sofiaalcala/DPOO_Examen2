f2b334edd97334cf561d1839385708e8a0b88b86
sofiaalcala <s.alcalag@uniandes.edu.co>
2024-10-30 08:32:47 -0500

    avance punto 1

--- Examen2/src/Parcial/Parcial.java
+++ Examen2/src/Parcial/Parcial.java
@@ -2,8 +2,19 @@ package Parcial;
 
 public class Parcial {
 	
-	public int raiz(int a, int b, int c) {
+	public static Double[] raiz(int a, int b, int c) throws Exception {
+		if (a == 0) {
+			throw new Exception("A debe ser mayor a 0, la ecuacion no es cuadratica.");}
+
+		double discriminante = (b * b) - 4*a*c;
+		if (discriminante < 0) {
+			throw new Exception("No tiene solución real, el discriminante debe ser mayor a 0.");}
 		
+		double raizsuma = ((-b + discriminante)/2*a);
+		double raizresta = ((-b - discriminante)/2*a);
+		
+		return new Double[] {raizsuma, raizresta};
+
 	}
 
 }
